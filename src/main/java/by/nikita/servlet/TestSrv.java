b28757364141bd429ce37b9e397d21497f77205c
nikita Kabak <nzkabak@gmail.com>
2018-05-21 22:04:05 +0300

    add new Entity

--- src/main/java/by/nikita/servlet/TestSrv.java
+++ src/main/java/by/nikita/servlet/TestSrv.java
@@ -13,17 +13,19 @@ import java.util.*;
 public class TestSrv {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        switch (request.getParameter("nameButton")){
+        switch (request.getParameter("nameButton")) {
             case "Create":
                 request.getRequestDispatcher("/by/nikita/jsp/homepageTest.jsp").forward(request, response);
             default:
                 request.getRequestDispatcher("/by/nikita/jsp/notFound.jsp").forward(request, response);
                 break;
         }
-    };
+    }
 
-    public void doPost( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        switch (request.getParameter("nameButton")){
+    ;
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        switch (request.getParameter("nameButton")) {
             case "create":
                 Test testCreate = new Test();
                 testCreate.setName(request.getParameter("name"));
@@ -44,26 +46,26 @@ public class TestSrv {
                 Test testUpdate = new Test();
                 Integer idUpdate = Integer.parseInt(request.getParameter("id"));
                 HbmDaoImp hbmDaoImpUpdate = new HbmDaoImp(Test.class);
-                testUpdate = (Test)hbmDaoImpUpdate.get(idUpdate);
+                testUpdate = (Test) hbmDaoImpUpdate.get(idUpdate);
                 testUpdate.setName(request.getParameter("name"));
                 testUpdate.setQantity(Integer.parseInt(request.getParameter("Qantity")));
                 hbmDaoImpUpdate.update(testUpdate);
-                testUpdate = (Test)hbmDaoImpUpdate.get(idUpdate);
+                testUpdate = (Test) hbmDaoImpUpdate.get(idUpdate);
                 HttpSession sessionUpdate = request.getSession();
-                sessionUpdate.setAttribute("testGet",testUpdate);
+                sessionUpdate.setAttribute("testGet", testUpdate);
                 request.getRequestDispatcher("/by/nikita/jsp/homepageTest.jsp").forward(request, response);
                 break;
             case "saveOrUpdate":
                 Test testSaveOrUpdate = new Test();
                 Integer idSaveOrUpdate = Integer.parseInt(request.getParameter("id"));
                 HbmDaoImp hbmSaveOrUpdate = new HbmDaoImp(Test.class);
-                testSaveOrUpdate = (Test)hbmSaveOrUpdate.get(idSaveOrUpdate);
+                testSaveOrUpdate = (Test) hbmSaveOrUpdate.get(idSaveOrUpdate);
                 testSaveOrUpdate.setName(request.getParameter("name"));
                 testSaveOrUpdate.setQantity(Integer.parseInt(request.getParameter("Qantity")));
                 hbmSaveOrUpdate.saveOrUpdate(testSaveOrUpdate);
-                testSaveOrUpdate = (Test)hbmSaveOrUpdate.get(idSaveOrUpdate);
+                testSaveOrUpdate = (Test) hbmSaveOrUpdate.get(idSaveOrUpdate);
                 HttpSession sessionSaveOrUpdate = request.getSession();
-                sessionSaveOrUpdate.setAttribute("testGet",testSaveOrUpdate);
+                sessionSaveOrUpdate.setAttribute("testGet", testSaveOrUpdate);
                 request.getRequestDispatcher("/by/nikita/jsp/homepageTest.jsp").forward(request, response);
                 request.getRequestDispatcher("/by/nikita/jsp/homepageTest.jsp").forward(request, response);
                 request.getRequestDispatcher("/by/nikita/jsp/homepageTest.jsp").forward(request, response);
@@ -72,7 +74,7 @@ public class TestSrv {
                 Test testDelete = new Test();
                 Integer idDelete = Integer.parseInt(request.getParameter("id"));
                 HbmDaoImp hbmDaoImpDelete = new HbmDaoImp(Test.class);
-                testDelete = (Test)hbmDaoImpDelete.get(idDelete);
+                testDelete = (Test) hbmDaoImpDelete.get(idDelete);
                 hbmDaoImpDelete.delete(testDelete);
                 /*HttpSession session = request.getSession();
                 session.setAttribute("testGet",testGet);*/
@@ -82,7 +84,7 @@ public class TestSrv {
                 Test testRemove = new Test();
                 Integer idRemove = Integer.parseInt(request.getParameter("id"));
                 HbmDaoImp hbmDaoImpRemove = new HbmDaoImp(Test.class);
-                testRemove = (Test)hbmDaoImpRemove.get(idRemove);
+                testRemove = (Test) hbmDaoImpRemove.get(idRemove);
                 hbmDaoImpRemove.remove(testRemove);
                 /*HttpSession session = request.getSession();
                 session.setAttribute("testGet",testGet);*/
@@ -113,21 +115,21 @@ public class TestSrv {
                 Orgerstatus orgerstatus = testOrder.getOrgerStatus();*/
 
                 //GET PRODUCT
-                Product testProduct = new Product();
+               /* Product testProduct = new Product();
                 Integer id = Integer.parseInt(request.getParameter("id"));
                 HbmDaoImp hbmDaoImpGet = new HbmDaoImp(Product.class);
                 testProduct = (Product)hbmDaoImpGet.get(id);
-                Productstatus productstatus = testProduct.getProductstatus();
+                Productstatus productstatus = testProduct.getProductstatus();*/
 
                 //USER
-                /*User testUser = new User();
+                User testUser = new User();
                 Integer id = Integer.parseInt(request.getParameter("id"));
                 HbmDaoImp hbmDaoImpGet = new HbmDaoImp(User.class);
-                testUser = (User)hbmDaoImpGet.get(id);
+                testUser = (User) hbmDaoImpGet.get(id);
                 //List<Backet> backetList = new ArrayList<>();
                 //backetList = testGet.getListBacket();
                 UserRole userRole = testUser.getUserRole();
-                Userstatus userstatus = testUser.getUserStatus();*/
+                Userstatus userstatus = testUser.getUserStatus();
 
                 HttpSession session = request.getSession();
 
@@ -143,14 +145,13 @@ public class TestSrv {
                 session.setAttribute("Orgerstatus",orgerstatus);*/
 
                 //GET PRODUCT
-                session.setAttribute("TestProduct",testProduct);
-                session.setAttribute("Productstatus",productstatus);
+                /*session.setAttribute("TestProduct",testProduct);
+                session.setAttribute("Productstatus",productstatus);*/
 
                 //GET USER
-                /*session.setAttribute("backetList",backetList);*/
-                /*session.setAttribute("TestUser",testUser);
-                session.setAttribute("UserRole",userRole);
-                session.setAttribute("Userstatus",userstatus);*/
+                session.setAttribute("TestUser", testUser);
+                session.setAttribute("UserRole", userRole);
+                session.setAttribute("Userstatus", userstatus);
 
                 request.getRequestDispatcher("/by/nikita/jsp/homepageTest.jsp").forward(request, response);
                 break;
@@ -158,9 +159,9 @@ public class TestSrv {
                 Test testRead = new Test();
                 Integer idRead = Integer.parseInt(request.getParameter("id"));
                 HbmDaoImp hbmDaoImpRead = new HbmDaoImp(Test.class);
-                testRead = (Test)hbmDaoImpRead.read(idRead);
+                testRead = (Test) hbmDaoImpRead.read(idRead);
                 HttpSession sessionRead = request.getSession();
-                sessionRead.setAttribute("testGet",testRead);
+                sessionRead.setAttribute("testGet", testRead);
                 request.getRequestDispatcher("/by/nikita/jsp/homepageTest.jsp").forward(request, response);
                 break;
             case "getAll":
@@ -176,7 +177,7 @@ public class TestSrv {
                 HbmDaoImp hbmDaoImpgetAll = new HbmDaoImp(Test.class);
                 listTest = hbmDaoImpgetAll.getAll(Test.class);*/
                 HttpSession sessiongetAll = request.getSession();
-                sessiongetAll.setAttribute("listTest",listTest);
+                sessiongetAll.setAttribute("listTest", listTest);
                 request.getRequestDispatcher("/by/nikita/jsp/homepageTest.jsp").forward(request, response);
                 break;
 
